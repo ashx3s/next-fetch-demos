3838cde813745e848b45386e10fecab6498c5241
ashx3s <ashlyn@lilyx.io>
2025-02-10 13:31:43 -0700

    character list server fetch

--- app/page.js
+++ app/page.js
@@ -1,25 +1,29 @@
-export default function Home() {
-  async function fetchCharacters() {
-    try {
-      const res = await fetch("https://api.disneyapi.dev/character");
-      const data = await res.json();
-      return data.data
-    } catch (err) {
-      console.error("Fetch Error: ", err);
+async function CharacterList() {
+  // fetch from api
+  let characters = [];
+  try {
+    const res = await fetch("https://api.disneyapi.dev/character");
+    if (!res.ok) {
+      throw new Error("Fetch response error");
     }
+    const data = await res.json();
+    characters = data.data;
+  } catch (error) {
+    console.error("Data Fetching Error: ", error);
   }
-const characters = fetchCharacters()
-console.log(characters)
-
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="">
-        
-        {/* <ul>
-          {characters.map((character, index) => {
-            return <li key={index}>{character.name}</li>;
-          })}
-        </ul> */}
+    <ul>
+      {characters.map((character, index) => {
+        return <li key={index}>{character.name}</li>;
+      })}
+    </ul>
+  );
+}
+export default function Home() {
+  return (
+    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+      <main>
+        <CharacterList />
       </main>
     </div>
   );
