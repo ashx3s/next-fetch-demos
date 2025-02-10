2fb739ed57f14b73933ae7a04824f487e450a4b4
ashx3s <ashlyn@lilyx.io>
2025-02-10 09:27:27 -0700

    client side data fetch

--- app/page.js
+++ app/page.js
@@ -1,26 +1,30 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [characters, setCharacters] = useState(null)
- 
+  const [characters, setCharacters] = useState([]);
+
   useEffect(() => {
     async function fetchCharacters() {
-      const res = await fetch('https://api.disneyapi.dev/character')
-      const data = await res.json()
-      setCharacters(data)
-      console.log(data)
+      const res = await fetch("https://api.disneyapi.dev/character");
+      const data = await res.json();
+       setCharacters(data.data);
     }
-    fetchCharacters()
-  }, [])
- 
+    fetchCharacters();
+  }, []);
+
+  console.log("Characters: ", characters);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="">
-        {/* TODO: Render Data  */}
+        <ul>
+          {characters.map((character, index) => {
+            return <li key={index}>{character.name}</li>
+          })}  
+        </ul>
       </main>
-
     </div>
   );
 }
