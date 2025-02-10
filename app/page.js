be4f4582be6b89f3a76ac5cf1f6e813211cf3d53
ashx3s <ashlyn@lilyx.io>
2025-02-10 10:00:11 -0700

    setup for server side fetch

--- app/page.js
+++ app/page.js
@@ -1,34 +1,25 @@
-"use client";
-
-import { useState, useEffect } from "react";
-
 export default function Home() {
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    async function fetchCharacters() {
-      try {
-
-        const res = await fetch("https://api.disneyapi.dev/character");
-        const data = await res.json();
-        setCharacters(data.data);
-      } catch(err) { 
-        console.error("Fetch Error: ", err)
-      }
+  async function fetchCharacters() {
+    try {
+      const res = await fetch("https://api.disneyapi.dev/character");
+      const data = await res.json();
+      return data.data
+    } catch (err) {
+      console.error("Fetch Error: ", err);
     }
-    fetchCharacters();
-  }, []);
-
-  console.log("Characters: ", characters);
+  }
+const characters = fetchCharacters()
+console.log(characters)
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="">
-        <ul>
+        
+        {/* <ul>
           {characters.map((character, index) => {
-            return <li key={index}>{character.name}</li>
-          })}  
-        </ul>
+            return <li key={index}>{character.name}</li>;
+          })}
+        </ul> */}
       </main>
     </div>
   );
