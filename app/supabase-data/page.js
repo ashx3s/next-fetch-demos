50e9801f86ea3d6702a2dc298d7839372b97fee5
ashx3s <ashlyn@lilyx.io>
2025-02-10 13:37:53 -0700

    star wars server fetch

--- app/supabase-data/page.js
+++ app/supabase-data/page.js
@@ -1,26 +1,26 @@
-"use client";
-import { useState, useEffect } from "react";
 import { supabase } from "../../lib/supabase";
-
-export default function StarWars() {
-  const [starWarsChars, setStarWarsChars] = useState([]);
-
-  useEffect(() => {
-    async function fetchStarWars() {
-      try {
-        let { data: characters, error } = await supabase
-          .from("characters")
-          .select("name");
-        setStarWarsChars(characters);
-      } catch (err) {
-        console.error("Fetch Error: ", err);
-      }
+async function StarWarsCharacters() {
+  let characters;
+  try {
+    let { data, error } = await supabase.from("characters").select("name");
+    if (error) {
+      throw new Error(error);
     }
-    fetchStarWars();
-  }, []);
-
-  console.log("Star Characters: ", starWarsChars);
-
+    characters = data;
+  } catch (error) {
+    console.error("Fetch Error: ", error);
+  }
+  return (
+    <div>
+      <ul>
+        {characters.map((character, index) => {
+          return <li key={index}>{character.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+export default function StarWars() {
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <main className=''>
@@ -31,13 +31,7 @@ export default function StarWars() {
             in class
           </p>
         </header>
-        <div>
-          <ul>
-            {starWarsChars.map((character, index) => {
-              return <li key={index}>{character.name}</li>;
-            })}
-          </ul>
-        </div>
+        <StarWarsCharacters />
       </main>
     </div>
   );
